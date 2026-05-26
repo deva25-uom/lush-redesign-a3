5dcc7b9a112e341b5d6024379ac7d5f92e8fc0d6
Devashree Kathale <devashree.kathale@gmail.com>
2026-05-26 17:30:37 +1000

    Added a small arrow element to the services homepage and also added an about and shop page

--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@ searchButton.addEventListener("click", function () {
   searchForm.classList.add("is-open");
   searchInput.focus();
 });
+
+
+
+// SERVICES //
+
+const scrollCue = document.querySelector(".scroll-cue");
+
+if (scrollCue) {
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 80) {
+      scrollCue.classList.add("is-hidden");
+    } else {
+      scrollCue.classList.remove("is-hidden");
+    }
+  });
+}
