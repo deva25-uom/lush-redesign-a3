8017f8ed6b5728f28c67163335e094599836b48b
Devashree Kathale <devashree.kathale@gmail.com>
2026-06-03 14:29:47 +1000

    Fixed page structure, and navigation

--- js/script.js
+++ js/script.js
@@ -2,23 +2,9 @@ const searchForm = document.querySelector(".search-form");
 const searchButton = document.querySelector(".search-toggle");
 const searchInput = document.querySelector("#site-search");
 
-searchButton.addEventListener("click", function () {
-  searchForm.classList.add("is-open");
-  searchInput.focus();
-});
-
-
-
-// SERVICES //
-
-const scrollCue = document.querySelector(".scroll-cue");
-
-if (scrollCue) {
-  window.addEventListener("scroll", function () {
-    if (window.scrollY > 80) {
-      scrollCue.classList.add("is-hidden");
-    } else {
-      scrollCue.classList.remove("is-hidden");
-    }
+if (searchForm && searchButton && searchInput) {
+  searchButton.addEventListener("click", function () {
+    searchForm.classList.add("is-open");
+    searchInput.focus();
   });
 }
